15d346aecff6b00c18da5df6b4ca190431a9c078
elk21 <danivlad6@gmail.com>
2017-12-24 23:35:58 +0300

    update

--- img_creation.py
+++ img_creation.py
@@ -1,14 +1,34 @@
 from PIL import Image, ImageDraw, ImageFont
 import requests
+from bs4 import BeautifulSoup
 import json
 
 URL = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
+
+
+def draw_text(d, pos, color=(0, 0, 0), text='', size=20):
+    fnt = ImageFont.truetype("data/arial.ttf", size)
+    d.text(pos, fill=color, text=text, font=fnt)
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_image(name):
+    url = 'https://coinmarketcap.com/'
+    r = requests.get(url)
+    soup = BeautifulSoup('lxml', r.text)
+    image = soup.find('a', text=name).parent.parent.find('img').attrs['src']
+    return image
+
 
 # Здесь, первый параметр это тип картинки, может быть:
 # 1 (черно-белый), L (монохромный, оттенки серого),
 # RGB, RGBA (RGB с альфа каналом), CMYK, YCbCr, I (32 bit Integer pixels),
 # F (32 bit Float pixels).
-img = Image.new("RGBA", (320, 320), (0, 0, 0, 0))
+img = Image.new("RGBA", (320, 160), (0, 0, 0, 0))
 draw = ImageDraw.Draw(img)
 
 rq = requests.get(URL).json()[0]
@@ -20,26 +40,14 @@ percent_change_1h = rq['percent_change_1h']
 percent_change_24h = rq['percent_change_24h']
 percent_change_7d = rq['percent_change_7d']
 
-
-font = ImageFont.truetype("data/arial.ttf", 40)
-draw.text((15, 10), fill=(0, 0, 0), text=symbol, font=font)
-
-font = ImageFont.truetype("data/arial.ttf", 24)
-draw.text((230, 15), fill=(0, 0, 0), text=price_usd, font=font)
-
-font = ImageFont.truetype("data/arial.ttf", 24)
-draw.text((2, 24), fill=(0, 0, 0), text=rank, font=font)
-
-font = ImageFont.truetype("data/arial.ttf", 24)
-draw.text((200, 40), fill=(0, 0, 0), text=percent_change_1h, font=font)
-
-font = ImageFont.truetype("data/arial.ttf", 24)
-draw.text((200, 60), fill=(0, 0, 0), text=percent_change_24h, font=font)
-
-font = ImageFont.truetype("data/arial.ttf", 24)
-draw.text((200, 80), fill=(0, 0, 0), text=percent_change_7d, font=font)
+draw_text(draw, (10, 0), text=symbol, size=50)
+draw_text(draw, (0, 0), text=rank)
+draw_text(draw, (200, 18), text=price_usd)
+draw_text(draw, (200, 80), text='1h:  ' + percent_change_1h + '%')
+draw_text(draw, (200, 100), text='24h: ' + percent_change_24h + '%')
+draw_text(draw, (200, 120), text='7d:  ' + percent_change_7d + '%')
+draw_text(draw, (200, 50), text=market_cap_usd)
 
 del draw
 img.save("test.png", "PNG")
 print(rq)
-
