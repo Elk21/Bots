56fbbb8f611a6d230c0252ae31a03d918460afdd
Efendiev Ruslan <rusefendi@gmail.com>
2017-12-25 00:09:00 +0300

    def main added

--- img_creation.py
+++ img_creation.py
@@ -1,7 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 import requests
 from bs4 import BeautifulSoup
-import json
 
 URL = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
 
@@ -24,36 +23,38 @@ def get_image(name):
     return image
 
 
-# Здесь, первый параметр это тип картинки, может быть:
-# 1 (черно-белый), L (монохромный, оттенки серого),
-# RGB, RGBA (RGB с альфа каналом), CMYK, YCbCr, I (32 bit Integer pixels),
-# F (32 bit Float pixels).
-img = Image.new("RGBA", (320, 160), (0, 0, 0, 0))
-draw = ImageDraw.Draw(img)
+def main():
+    # Здесь, первый параметр это тип картинки, может быть:
+    # 1 (черно-белый), L (монохромный, оттенки серого),
+    # RGB, RGBA (RGB с альфа каналом), CMYK, YCbCr, I (32 bit Integer pixels),
+    # F (32 bit Float pixels).
+    img = Image.new("RGBA", (320, 160), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
 
-rq = requests.get(URL).json()[0]
-_id = rq['id']
-price_usd = rq['price_usd']
-rank = rq['rank']
-symbol = rq['symbol']
-market_cap_usd = rq['market_cap_usd']
-percent_change_1h = rq['percent_change_1h']
-percent_change_24h = rq['percent_change_24h']
-percent_change_7d = rq['percent_change_7d']
-img = Image.open(get_image(_id))
+    rq = requests.get(URL).json()[0]
+    _id = rq['id']
+    price_usd = rq['price_usd']
+    rank = rq['rank']
+    symbol = rq['symbol']
+    market_cap_usd = rq['market_cap_usd']
+    percent_change_1h = rq['percent_change_1h']
+    percent_change_24h = rq['percent_change_24h']
+    percent_change_7d = rq['percent_change_7d']
+    img = Image.open(get_image(_id))
+
+    draw_text(draw, (10, 0), text=symbol, size=50)
+    draw_text(draw, (0, 0), text=rank)
+    draw_text(draw, (200, 18), text=price_usd)
+    draw_text(draw, (200, 80), text='1h:  ' + percent_change_1h + '%')
+    draw_text(draw, (200, 100), text='24h: ' + percent_change_24h + '%')
+    draw_text(draw, (200, 120), text='7d:  ' + percent_change_7d + '%')
+    draw_text(draw, (100, 50), text=market_cap_usd)
+    draw.bitmap((50, 50), img.bitmap(), fill=None)
+
+    del draw
+    img.save("test.png", "PNG")
+    print(rq)
 
 
-draw_text(draw, (10, 0), text=symbol, size=50)
-draw_text(draw, (0, 0), text=rank)
-draw_text(draw, (200, 18), text=price_usd)
-draw_text(draw, (200, 80), text='1h:  ' + percent_change_1h + '%')
-draw_text(draw, (200, 100), text='24h: ' + percent_change_24h + '%')
-draw_text(draw, (200, 120), text='7d:  ' + percent_change_7d + '%')
-draw_text(draw, (100, 50), text=market_cap_usd)
-draw.bitmap((50, 50), img.bitmap(), fill=None)
-
-
-
-del draw
-img.save("test.png", "PNG")
-print(rq)
+if __name__ == '__main__':
+    main()
