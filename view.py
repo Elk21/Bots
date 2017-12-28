e77043e37e72737cbf3a98969a7ac8440a47af31
Efendiev Ruslan <rusefendi@gmail.com>
2017-12-28 21:40:54 +0300

    img folder added

--- view.py
+++ view.py
@@ -7,5 +7,5 @@ class View:
     tb.set_webhook(url=mics.webhook_url_path)
 
     def send(self, chat_id):
-        with open('single_coin.png', 'rb') as photo:
+        with open('img/single_coin.png', 'rb') as photo:
             self.tb.send_photo(chat_id=chat_id, photo=photo)
